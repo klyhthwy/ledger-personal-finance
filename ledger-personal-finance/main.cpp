d770d41fb0566b2f9ef7181a0cd7c22e63589248
Kelly Hathaway <klyhthwy@gmail.com>
2017-05-07 11:27:39 -0600

    Saving xcode project start.

---
+++ ledger-personal-finance/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  ledger-personal-finance
+//
+//  Created by Kelly Hathaway on 5/7/17.
+//  Copyright © 2017 Kelly Hathaway. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
